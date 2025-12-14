ad7de52bde6c409b9d3084e2578f4c1d5c7f6815
kinasant <kinasant@yahoo.com>
2025-12-14 11:40:57 +0530

    Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,17 +5,27 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js"
 // const express = require("express");
+import path, { dirname } from 'path'
 import cors from "cors"
-
+const __dirname = path.resolve()
 dotenv.config();
 
 const app = express();
 connectDB();
 app.use(express.json())
+if (process.env.NODE_ENV!=="production"){
 app.use(cors({origin:"http://localhost:5173"}))
+}
 app.use(rateLimiter)
 app.use("/api/notes",notesRoutes);
+if (process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.use("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
 
+
+}
 
 app.listen(5001,()=>{console.log("Server Started");});
 
